dd304823d8f1052111148782cb5cda68a634789a
Rao Hamza Tariq <147372279+RaoHamzaTariq@users.noreply.github.com>
2024-09-28 23:51:17 +0500

    Update page.tsx

--- Ecommerce Website/src/app/checkout/page.tsx
+++ Ecommerce Website/src/app/checkout/page.tsx
@@ -1,5 +1,9 @@
+import React from 'react'
+
 const CheckOut = () => {
-  return {
-    <h1>This is Checkout Page</h1>
-  }
+  return (
+    <div>This is the checkout page</div>
+  )
 }
+
+export default CheckOut
