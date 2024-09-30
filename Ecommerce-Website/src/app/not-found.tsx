374022019ad33316d442c3d83bbf03e28ea2c88f
Rao Hamza Tariq <raohamza6767@gmail.com>
2024-09-30 10:37:11 +0500

    Updated

---
+++ Ecommerce-Website/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='w-screen h-screen justify-center items-center'>
+      <h1>Not Found</h1>
+    </div>
+  );
+}
+
+export default NotFound;
