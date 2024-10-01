1a8439de4ebdb84d9c5cd32ae1c708b7b40985bd
Rao Hamza Tariq <raohamza6767@gmail.com>
2024-10-01 15:51:39 +0500

    Updated

--- ecommerce-project/src/app/products/[productByCategory]/[productDetail]/page.tsx
+++ ecommerce-project/src/app/products/[productByCategory]/[productDetail]/page.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
+import { useParams } from 'next/navigation';
 
 const page = () => {
+  const params = useParams()
+  const id = params.productDetail
+  console.log(id);
+  
   return (
     <div>
-      
+      {id}
     </div>
   );
 }
--- ecommerce-project/src/app/products/[productByCategory]/page.tsx
+++ ecommerce-project/src/app/products/[productByCategory]/page.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
       
+      {"Product Category"}
+      <Link href={"/products/dhse/ewwe"}></Link>
     </div>
   );
 }
