5c2ccf222925b379be27b9ae1ac91667196b9174
Rao Hamza Tariq <raohamza6767@gmail.com>
2024-10-02 08:54:02 +0500

    Updated

--- Porfolio Website/src/app/page.tsx
+++ Porfolio Website/src/app/page.tsx
@@ -4,53 +4,15 @@ import { StackCarousal } from "@/components/StackCarousel";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-// import {FaRobot,FaCode,FaComments,FaCheckSquare } from "react-icons/fa";
+import {FaRobot,FaCode,FaComments,FaCheckSquare } from "react-icons/fa";
 import FAQS from "../../public/images/FAQS.svg"
-// import { RiDashboard2Line } from "react-icons/ri";
-// import { MdInsights } from "react-icons/md";
+import { RiDashboard2Line } from "react-icons/ri";
+import { MdInsights } from "react-icons/md";
 import FAQs from "@/components/FAQs";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-// import { IconType } from "react-icons/lib";
+import { IconType } from "react-icons/lib";
 import React from "react";
 
-interface WhyChooseMe {
-  // icon:JSX.Element
-  title:string
-  desc:string
-}
-
-export const whyChooseMe : WhyChooseMe[] = [
-      {
-          // icon: <MdInsights className="h-8 w-8 text-primary"/>,
-          title: "Insightful Data Analysis",
-          desc: "With my data analysis expertise, I can uncover valuable insights from your data, helping you understand trends, customer behavior, and operational efficiency for informed decision-making."
-      },
-      {
-          // icon: <FaRobot className="h-8 w-8 text-primary"/>,
-          title: "Machine Learning Expertise",
-          desc: "I specialize in developing machine learning models that predict outcomes and optimize processes, creating tailored solutions for forecasting sales or improving customer experiences."
-      },
-      {
-          // icon: <RiDashboard2Line className="h-8 w-8 text-primary"/>,
-          title: "Engaging Data Visualization",
-          desc: "I create clear and interactive visualizations that make complex data easy to understand, using tools like Power BI to help you track key metrics and tell a compelling data story."
-      },
-      {
-          // icon: <FaCode className="h-8 w-8 text-primary"/>,
-          title: "Custom Web Development",
-          desc: "I build websites that are visually appealing, functional, and user-friendly, whether you need a simple site or a complex web application tailored to your needs."
-      },
-      {
-          // icon: <FaComments className="h-8 w-8 text-primary"/>,
-          title: "Strong Communication",
-          desc: "I prioritize open communication throughout our collaboration, ensuring you are always updated on project progress and available to answer any questions."
-      },
-      {
-          // icon: <FaCheckSquare className="h-8 w-8 text-primary"/>,
-          title: "Commitment to Quality",
-          desc: "I am dedicated to delivering high-quality results, putting in the effort needed to ensure your project meets the highest standards."
-      }
-  ];
 
 
 export default function Home() {
@@ -59,9 +21,9 @@ export default function Home() {
 <main>
 <section className="grid grid-cols-1 md:grid-cols-2 lg:gap-80 overflow-hidden    items-center justify-between lg:pt-16 sm:pt-40 pt-40  overflow-x-hidden h-screen">
   <div className=" max-w-lg ml-20 flex flex-col gap-5">
-    <h3>I&apos;m Hamza</h3>
+    <h3>{`I'm Hamza`}</h3>
     <h1>Data Analyst and Web Developer</h1>
-    <p>I am a skilled Data Analyst, Web Developer, and Power BI Developer, specializing in transforming data into insights and building user-friendly web solutions to help you achieve your goals.</p>
+    <p>{`I am a skilled Data Analyst, Web Developer, and Power BI Developer, specializing in transforming data into insights and building user-friendly web solutions to help you achieve your goals.`}</p>
     <div>
       <Link href={"/Portfolio/All"}><Button>View my Projects</Button></Link>  
     </div>
@@ -71,7 +33,7 @@ export default function Home() {
 <section className="my-16">
   <FeatureProjectsCarousal/>
 </section>
-<section className="w-full py-20 md:py-32 bg-[#1a1b1e] text-white">
+        <section className="w-full py-20 md:py-32 bg-[#1a1b1e] text-white">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-8 text-center">
               <div className="space-y-4">
@@ -79,25 +41,66 @@ export default function Home() {
                 Why Choose Me for Your Projects
                 </h2>
                 <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                I offer a wide range of services, including data science, data analysis, web development, and Power BI dashboard creation. No matter what your project needs, I have the skills to help you succeed.
-
+                {`I offer a wide range of services, including data science, data analysis, web development, and Power BI dashboard creation. No matter what your project needs, I have the skills to help you succeed.`}
                 </p>
               </div>
-              <ul className="mx-auto grid max-w-5xl items-start gap-8 sm:grid-cols-2 md:gap-12 lg:grid-cols-3">
-                
-                {whyChooseMe.map((quality:WhyChooseMe)=>(
-
-                <div key={quality.title} className="grid gap-4">
+              <div className="mx-auto grid max-w-5xl items-start gap-8 sm:grid-cols-2 md:gap-12 lg:grid-cols-3">
+                <div className="grid gap-4">
                   <div className="flex items-center gap-4">
-                    {/* {quality.icon} */}
-                    <h3 className="text-xl font-bold">{quality.title}</h3>
+                    <MdInsights className="h-8 w-8 text-primary" />
+                    <h3 className="text-xl font-bold">Insightful Data Analysis</h3>
                   </div>
-                  <p className="text-muted-foreground">{quality.desc}</p>
+                  <p className="text-muted-foreground">
+                    {`With my data analysis expertise, I can uncover valuable insights from your data, helping you understand trends, customer behavior, and operational efficiency for informed decision-making.`}
+                  </p>
                 </div>
-
-                ))}
-          </ul>
-          </div>
+                <div className="grid gap-4">
+                  <div className="flex items-center gap-4">
+                    <FaCode className="h-8 w-8 text-primary" />
+                    <h3 className="text-xl font-bold">Machine Learning Expertise</h3>
+                  </div>
+                  <p className="text-muted-foreground">
+                  {`I specialize in developing machine learning models that predict outcomes and optimize processes, creating tailored solutions for forecasting sales or improving customer experiences.`}
+                  </p>
+                </div>
+                <div className="grid gap-4">
+                  <div className="flex items-center gap-4">
+                    <FaRobot className="h-8 w-8 text-primary" />
+                    <h3 className="text-xl font-bold">Engaging Data Visualization</h3>
+                  </div>
+                  <p className="text-muted-foreground">
+                    {`I create clear and interactive visualizations that make complex data easy to understand, using tools like Power BI to help you track key metrics and tell a compelling data story.`}
+                  </p>
+                </div>
+                <div className="grid gap-4">
+                  <div className="flex items-center gap-4">
+                    <FaCheckSquare className="h-8 w-8 text-primary" />
+                    <h3 className="text-xl font-bold">Custom Web Development</h3>
+                  </div>
+                  <p className="text-muted-foreground">
+                    {`I build websites that are visually appealing, functional, and user-friendly, whether you need a simple site or a complex web application tailored to your needs.`}
+                  </p>
+                </div>
+                <div className="grid gap-4">
+                  <div className="flex items-center gap-4">
+                    <FaComments className="h-8 w-8 text-primary" />
+                    <h3 className="text-xl font-bold">Strong Commnunication</h3>
+                  </div>
+                  <p className="text-muted-foreground">
+                    {`I prioritize open communication throughout our collaboration, ensuring you are always updated on project progress and available to answer any questions.`}
+                  </p>
+                </div>
+                <div className="grid gap-4">
+                  <div className="flex items-center gap-4">
+                    <RiDashboard2Line className="h-8 w-8 text-primary" />
+                    <h3 className="text-xl font-bold">Commitment to Quality</h3>
+                  </div>
+                  <p className="text-muted-foreground">
+                    {`I am dedicated to delivering high-quality results, putting in the effort needed to ensure your project meets the highest standards.`}
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
         <section>
@@ -107,7 +110,7 @@ export default function Home() {
 <section className="my-32 space-y-28">
   <div className="flex flex-col gap-5 justify-center text-center items-center">
     <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl  ">Choose Your Services Plan</h1>
-    <p className="max-w-[900px] text-gray-700 dark:text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">Select the perfect plan tailored to your needs, whether you&apos;re looking for expert data analysis, cutting-edge data science solutions, or innovative web development services.</p>
+    <p className="max-w-[900px] text-gray-700 dark:text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">{`Select the perfect plan tailored to your needs, whether you're looking for expert data analysis, cutting-edge data science solutions, or innovative web development services.`}</p>
   </div>
   <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-center gap-10 items-center container mx-auto">
     <div>
