72f73452c31a326182e4b260f3e484683a202a38
Rao Hamza Tariq <raohamza6767@gmail.com>
2024-10-12 13:21:04 +0500

    Updated

---
+++ Ecommerce-Website/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-screen py-52 flex justify-center items-center text-2xl'>This page is under development</div>
+  )
+}
+
+export default About
